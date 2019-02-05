c688e784fbe961f6c69ed9a61256858b380ff44f
Luídi Matheus Silva de Oliveira <luidi_mso@outlook.com>
2019-02-05 16:30:15 -0200

    Get

--- 2-API/dist/main.js
+++ 2-API/dist/main.js
@@ -1,1 +1,14 @@
-console.log("Uia");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const restify = require("restify");
+const server = restify.createServer({
+    name: 'teste-api',
+    version: '1.0.0'
+});
+server.get('/teste', (req, resp, next) => {
+    resp.json({ message: 'Alou' });
+    return next();
+});
+server.listen(3000, () => {
+    console.log("Tá rodando");
+});
--- 2-API/main.ts
+++ 2-API/main.ts
@@ -1,1 +1,15 @@
-console.log("Uia");
+import * as restify from 'restify'
+
+const server = restify.createServer({
+  name: 'teste-api',
+  version: '1.0.0'
+})
+
+server.get('/teste', (req, resp, next) => {
+  resp.json({message: 'Alou'})
+  return next()
+})
+
+server.listen(3000, ()=>{
+  console.log("Tá rodando")
+})
