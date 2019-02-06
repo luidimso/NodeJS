a748c34557d202a65541eee83101e4418c36a8e3
Luídi Matheus Silva de Oliveira <luidi_mso@outlook.com>
2019-02-06 14:14:06 -0200

    Put

--- 2-API/users/users.router.ts
+++ 2-API/users/users.router.ts
@@ -34,6 +34,21 @@ class UsersRouter extends Router {
         return next()
       })
     })
+
+
+    application.put('/users/:id', (req, resp, next)=>{
+      const options = {overwrite: true}
+      User.update({_id:req.params.id}, req.body, options).exec().then(result=>{ //Atualização total
+        if(result.n){
+          return User.findById(req.params.id)
+        }else{
+          resp.send(404)
+        }
+      }).then(user=>{
+        resp.json(user)
+        return next()
+      })
+    })
   }
 }
 
